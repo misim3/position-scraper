ef150ebd9b9f059a6b7bb496182af1f597b6a65d
misim3 <misim@student.42seoul.kr>
2023-09-21 11:53:45 +0900

    Fix: findOpenPositionPage

--- main.py
+++ main.py
@@ -45,14 +45,14 @@ def checkPage(pageUrl):
 # 회사의 영어 이름은 모두 12자리이내라고 가정하자.
 # 재귀 12번 실행하면서 26자리의 알파벳을 반복한다. => 엄청난 비효율 26**12 = 95,428,956,661,682,176
 # @이 엄청난 비효율을 어떻게 해소할 수 있을까?
-# @추가적으로 존재하는 페이지 정보를 파일에 저장하고 파일에 존재하지 않는 페이지 정보를 새롭게 확인하자.
+# @추가적으로 존재하는 페이지 정보를 db에 저장하고 새로운 페이지 확인하면 추가하자.
 # @그리고 해당 회사의 채용 공고를 확인하는 것에 대한 주기도 따로 고려하자.
 
 def makeUrl(pageUrl, n):
     # 도메인 이름은 대소문자 구분이 없기 때문에 소문자만 사용한다.
     # '', ""의 차이
     letters = 'abcdefghijklmnopqrstuvwxyz'
-    if n < 2:
+    if n < 12:
         for i in letters:
             makeUrl(pageUrl + i, n + 1)
             #checkPage(pageUrl + '.career.greetinghr.com')
@@ -63,14 +63,18 @@ def findOpenPositionPage(pageUrl):
     pattern = re.compile(r'.*(apply|career).*')
     # findAll이 아닌 이유는 그리팅 하이어 서비스의 경우 여러 페이지에서 공고를 확인할 수 있다.
     # 따라서 걸리는 것 하나에서만 찾으면 된다.
-    # @apply나 career 페이지가 없고 바로 공고만 존재하는 경우는 어떻게 처리할 것인지 고려
-    for link in bs.find('a', href=pattern):
+    # apply나 career 페이지가 없고 바로 공고만 존재하는 경우는 어떻게 처리할 것인지 고려
+    link = bs.find('a', href=pattern)
+    if link:
         getPositionData(link)
+    else:
+        getPositionData(pageUrl)
 
 def getPositionData(pageUrl):
-    #동시에 driver 사용이 불가능하다.
-    #@따라서 병렬 처리를 해주거나 아예 다른 방법을 사용해야 한다.
-    #@requests와 selenium을 이용한 방식 등 에 대해 자세히 알아볼 필요가 있다.
+    # 동시에 driver 사용이 불가능하다.
+    # 따라서 병렬 처리를 해주거나 아예 다른 방법을 사용해야 한다. -> 기존에는 findOpenPositionData와 getPositionData가 모두 selenium을
+    # 사용했었는데, findOpenPositionData를 requests로 변경했다.
+    # requests와 selenium을 이용한 방식 등 에 대해 자세히 알아볼 필요가 있다.
     driver = webdriver.Edge()
     driver.get(pageUrl)
     details = driver.find_elements(By.TAG_NAME, "a")
